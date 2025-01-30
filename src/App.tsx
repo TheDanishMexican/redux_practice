22b83191e57a0de0b587f13ca8b01d7bb75e3b04
Daniel Javier <daja@ageras.com>
2025-01-30 09:30:19 +0100

    did another slice

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import "./App.css";
+import ColorChanger from "./components/ColorChanger";
 import Counter from "./components/Counter";
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
       <div>
         <h1>Redux test</h1>
         <Counter />
+        <ColorChanger />
       </div>
     </>
   );
--- src/state/store.ts
+++ src/state/store.ts
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import counterReducer from "./counter/counterSlice.ts";
+import colorChangeReducer from "./color/colorChangeSlice.ts";
 
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
+    colorChanger: colorChangeReducer,
   },
 });
 
