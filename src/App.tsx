c4aa339839b7d1da98f73f1728b6ecc558df622b
Daniel Javier <daja@ageras.com>
2025-01-30 08:58:14 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import Counter from "./components/Counter";
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1>Redux test</h1>
+        <Counter />
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Counter.tsx
@@ -0,0 +1,27 @@
+import { useDispatch, useSelector } from "react-redux";
+import { AppDispatch, RootState } from "../state/store";
+import {
+  decrement,
+  increment,
+  incrementByAmount,
+  incrementAsync,
+} from "../state/counter/counterSlice";
+
+const Counter = () => {
+  const count = useSelector((state: RootState) => state.counter.value);
+  const dispatch = useDispatch<AppDispatch>();
+
+  return (
+    <>
+      <div>
+        <h2>{count}</h2>
+        <div>
+          <button onClick={() => dispatch(incrementAsync(10))}>Add</button>
+          <button onClick={() => dispatch(decrement())}>Subtract</button>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Counter;
